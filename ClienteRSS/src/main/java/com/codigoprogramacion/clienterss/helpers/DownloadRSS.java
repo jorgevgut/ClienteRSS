7032cc107d9e747f5543f5381c62a87e87edfb52
xymind <jogeavi914@gmail.com>
2013-11-05 09:37:10 -0600

    Implementando download con httpUrlConnection

--- ClienteRSS/src/main/java/com/codigoprogramacion/clienterss/helpers/DownloadRSS.java
+++ ClienteRSS/src/main/java/com/codigoprogramacion/clienterss/helpers/DownloadRSS.java
@@ -1,13 +1,11 @@
 package com.codigoprogramacion.clienterss.helpers;
 
-import java.io.ByteArrayOutputStream;
+
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.StringWriter;
-import java.io.Writer;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import android.util.Log;
 
 /**
  * Created by jorch on 11/4/13.
@@ -16,46 +14,41 @@ public class DownloadRSS {
 
      public static String getRSSfromURL(String url)
      {
-         HttpURLConnection con;
-         URL miurl;
-         String rss="def";
-         InputStream entrada=null;
+         HttpURLConnection conexion;
+         URL miUrl;
+         InputStream entrada;
+         File xml;
+         FileOutputStream fileOut;
+         final int bufferSize = 1024;
+         byte[] buffer;
          try{
-             miurl = new URL(url);
-             con = (HttpURLConnection)miurl.openConnection();
-             con.setReadTimeout(15000);
-             con.setConnectTimeout(15000);
-             con.setDoInput(true);
-             con.setReadTimeout(15000);
+             miUrl = new URL(url);
+             conexion = (HttpURLConnection)miUrl.openConnection();
+             conexion.setReadTimeout(10000);
+             conexion.setConnectTimeout(10000);
+             conexion.setDoInput(true);
+             conexion.setRequestMethod("GET");
+             conexion.connect();
 
-             con.connect();
+             entrada = conexion.getInputStream();
+             xml = new File("/storage/sdcard0/rss.xml");
+             fileOut = new FileOutputStream(xml);
 
-             entrada = con.getInputStream();
-
-             Log.d("CLIENTE RSS","Termina configuración de conexión");
-             //Lectura de rss
-             final int size = 1024*1;
-
-             byte[] buffer = new byte[size];
-             ByteArrayOutputStream out = new ByteArrayOutputStream();
-
-             while(entrada.read(buffer)>0)
+             int bytesAEscribir;
+             buffer = new byte[bufferSize];
+             while ((bytesAEscribir = entrada.read(buffer))>0)
              {
-                 out.write(buffer,0,size);
+                 fileOut.write(buffer,0,bytesAEscribir);
              }
+             entrada.close();
+             fileOut.close();
+             conexion.disconnect();
 
 
-
-            out.close();
-            entrada.close();
-            con.disconnect();
-
-            rss = new String(out.toByteArray(),"UTF-8");
-            return rss;
          }catch(Exception e){
-             Log.d("CLIENTE RSS 'ERROR'",e.getMessage());}
 
-         Log.d("CLIENTE RSS","no se bajo la info");
-         return rss;
+         }
+
+         return null;
      }
 }
