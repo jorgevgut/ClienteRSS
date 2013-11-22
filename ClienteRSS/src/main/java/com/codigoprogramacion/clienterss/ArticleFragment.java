d9c1fe75e802e22bd6b7d510b989b3b34af058d8
xymind <jogeavi914@gmail.com>
2013-11-22 14:03:34 -0600

    mas cosas

---
+++ ClienteRSS/src/main/java/com/codigoprogramacion/clienterss/ArticleFragment.java
@@ -0,0 +1,22 @@
+package com.codigoprogramacion.clienterss;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by jorge on 11/22/13.
+ */
+public class ArticleFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(
+                R.layout.fragment_article, container, false);
+
+        return rootView;
+    }
+}
